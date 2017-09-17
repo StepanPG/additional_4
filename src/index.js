19c136e79af355283cc4476659c45b76446c4922
StepanPG <stepan-mandarin@yandex.ru>
2017-09-17 12:52:00 +0300

    multiply implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var firstArr = first.split('').reverse(),
+      secondArr = second.split('').reverse();
+
+    var result = [],
+        firstArrIndex,
+        secondArrIndex,
+        remainder = 0;
+
+    for(firstArrIndex = 0; firstArrIndex < firstArr.length; firstArrIndex++){
+
+        for(secondArrIndex = 0; secondArrIndex < secondArr.length || remainder > 0; secondArrIndex++){
+            result[firstArrIndex + secondArrIndex] = (remainder += (result[firstArrIndex + secondArrIndex] || 0) + firstArr[firstArrIndex] * (secondArr[secondArrIndex] || 0)) % 10;
+            remainder = Math.floor(remainder / 10);
+        }
+
+    }
+
+    return result.reverse().join('');
 }
